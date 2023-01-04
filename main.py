439fc94792f348acdfea803cbd8b63984d3ed250
YeSeul-SR <yeseul@seoulrobotics.org>
2023-01-04 18:57:56 +0900

    organization

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ import os
 import argparse
 import sys
 import time
-
 import psutil
-import threading
 import pandas
-import logging
 from datetime import datetime
 
 def check_GPU_info():
@@ -20,8 +17,9 @@ def check_GPU_info():
     return information_list
 
 def check_CPU_temp():
-    temp = []
-    label = []
+    usage = psutil.cpu_percent(interval=None)
+    temp = [usage]
+    label = ["usage"]
     if not hasattr(psutil, "sensors_temperatures"):
         sys.exit("platform not supported")
     temps = psutil.sensors_temperatures()
@@ -42,14 +40,6 @@ def check_CPU_temp():
 def check_CPU_usage(time):
     return psutil.cpu_percent(interval=time)
 
-def check_memory_usage():
-    mem = psutil.virtual_memory()
-    print('memory usage :', mem[2], '%')
-
-def check_disk_usage():
-    disk = psutil.disk_usage('/')
-    print('disk usage :', disk.percent, '%')
-
 def check_etc_usage():
     info = []
 
@@ -66,14 +56,22 @@ def parse_arguments():
     parser.add_argument('--time', type=int, default=30)
     return parser.parse_args()
 
+def save_file(GPU_df,CPU_df,usage_df):
+    GPU_df.to_csv("./data/GPU.csv")
+    CPU_df.to_csv("./data/CPU.csv")
+    usage_df.to_csv("./data/usage_info.csv")
+
 def main(timer):
-    logging.info("start to check LPU-B information")
     now = datetime.now()
     date = now.strftime('%Y-%m-%d %H:%M:%S')
+    print(f"{date} start to check LPU-B status")
     GPU_df = pandas.DataFrame(check_GPU_info(), columns=[date], index=["temperature", "usage", "memory usage"])
     cpu_label, cpu_temp = check_CPU_temp()
     CPU_df = pandas.DataFrame(cpu_temp, columns=[date], index=cpu_label)
     usage_df = pandas.DataFrame(check_etc_usage(), columns=[date], index=["memory", "disk"])
+    save_file(GPU_df,CPU_df,usage_df)
+    time.sleep(timer)
+
     while True:
         now = datetime.now()
         date = now.strftime('%Y-%m-%d %H:%M:%S')
@@ -82,14 +80,12 @@ def main(timer):
         CPU_df[date] = cpu_temp
         usage_df[date] = check_etc_usage()
 
-        GPU_df.to_csv("./data/GPU.csv")
-        CPU_df.to_csv("./data/CPU_temp.csv")
-        usage_df.to_csv("./data/usage_info.csv")
-        print(now, "saving")
-        logging.info("saved file")
+        save_file(GPU_df,CPU_df,usage_df)
+        print(f"{date}, saved file")
+
         time.sleep(timer)
+
 
 if __name__ == '__main__':
     parmeter = parse_arguments()
-    logging.info(f"set time is {parmeter.time}")
     main(parmeter.time)
